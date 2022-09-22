426b6b3d28ebe5774230b2e0b6de5db6963ba6f0
SpaceChae <enaenen@naver.com>
2022-09-22 17:50:12 +0900

    algo header add

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,13 +2,15 @@
 
 #include <iostream>
 #include <exception>
+#include <algorithm>
 #include <vector>
 #include <list>
 #include <set>
 
 template <typename T>
 typename T::iterator easyfind( T& sources, const int target ) {
-	typename T::iterator ret = find( sources.begin(), sources.end(), target );
+	typename T::iterator ret =
+		std::find( sources.begin(), sources.end(), target );
 	if ( ret == sources.end() )
 		throw std::runtime_error( "target not found in element" );
 	return ret;
@@ -16,7 +18,8 @@ typename T::iterator easyfind( T& sources, const int target ) {
 
 template <typename T>
 const typename T::iterator easyfind( const T& sources, const int target ) {
-	typename T::iterator ret = find( sources.begin(), sources.end(), target );
+	typename T::iterator ret =
+		std::find( sources.begin(), sources.end(), target );
 	if ( ret == sources.end() )
 		throw std::runtime_error( "target not found in element" );
 	return ret;
