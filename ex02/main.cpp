86ae431d12c73d42096afdb5d8a0cbac6455a280
Woo Joo Chae <enaenen@naver.com>
2022-09-23 01:59:37 +0900

    main test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,21 +1,54 @@
 #include "MutantStack.hpp"
 
 int main( void ) {
-	std::cout << "========== Mandatory MutantStack Test =========="
-			  << std::endl;
-	MutantStack<int> stack;
-
-	stack.push( 10 );
-	stack.push( 150 );
-	std::cout << "stack.top(): " << stack.top() << std::endl;
-	stack.pop();
-	std::cout << "stack.size(): " << stack.size() << std::endl;
-	stack.push( 1 );
-	stack.push( 2 );
-	stack.push( 3 );
-	MutantStack<int>::iterator it = stack.begin();
-	MutantStack<int>::iterator ite = stack.end();
-	++it;
-	--it;
-	for ( ; it != ite; ++it ) std::cout << *it << std::endl;
+	{
+		std::cout << "========== Mandatory MutantStack Test =========="
+				  << std::endl;
+		MutantStack<int> mstack;
+		mstack.push( 5 );
+		mstack.push( 17 );
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push( 3 );
+		mstack.push( 5 );
+		mstack.push( 737 );
+		//[...]
+		mstack.push( 0 );
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while ( it != ite ) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s( mstack );
+	}
+	{
+		{
+			std::cout << "========= String test ==========" << std::endl;
+			MutantStack<std::string> mstack;
+			mstack.push( "abcd" );
+			mstack.push( "efgh" );
+			std::cout << "mstack.top(): " << mstack.top() << std::endl;
+			mstack.pop();
+			std::cout << "mstack.size(): " << mstack.size() << std::endl;
+			mstack.push( "xxx" );
+			mstack.push( "yyyy" );
+			mstack.push( "zzzz" );
+			mstack.push( "this is mutant stack" );
+			MutantStack<std::string>::iterator it = mstack.begin();
+			MutantStack<std::string>::iterator ite = mstack.end();
+			++it;
+			--it;
+			while ( it != ite ) {
+				std::cout << *it << std::endl;
+				++it;
+			}
+			std::stack<std::string> s( mstack );
+			std::cout << std::endl;
+		}
+	}
+	return 0;
 }
